11ec20a10eb1f3f83e6fcdcb9929f6de37cbfb9a
GustavoMMFigaro <gustavomareti@edu.unifil.br>
2025-07-04 22:57:44 -0300

    Third commit - ViewModel

---
+++ app/src/main/java/com/example/pf_iak/TarefaViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.pf_iak
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.pf_iak.data.AppDatabase
+import com.example.pf_iak.data.Tarefa
+import kotlinx.coroutines.launch
+
+class TarefaViewModel(application : Aplication) : AndroidViewModel(application) {
+    private val dao = AppDatabase.getDatabase(application).tarefaDao()
+    val todas: LiveData<List<Tarefa>> = dao.getTodas()
+
+    fun inserir(tarefa : Tarefa) = viewModelScope.launch {
+        dao.inserir(tarefa)
+    }
+
+    fun atualizar(tarefa : Tarefa) = viewModelScope.launch {
+        dao.atualizar(tarefa)
+    }
+
+    fun deletar(tarefa : Tarefa) = viewModelScope.launch {
+        dao.deletar(tarefa)
+    }
+}
